45ba2eabdb06f19e832453da89eea4a4950c27bb
Joshua Karjala-Svendsen <joshua@fluxuries.com2>
2019-09-04 15:09:45 +0200

    permissions

--- node/install.sh
+++ node/install.sh
@@ -13,7 +13,8 @@ if test ! $(which yarn); then
   else
     curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
     echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-    sudo apt-get update && sudo apt-get install -y --no-install-recommends yarn
+    sudo apt-get update
+    sudo apt-get install -y --no-install-recommends yarn
   fi
 fi
 
