f7d6838d64f99fdf62e1400412ecf98ab779f37b
Joshua Karjala-Svendsen <joshua@fluxuries.com2>
2019-09-05 13:46:41 +0200

    better paths

--- system/_path.zsh
+++ system/_path.zsh
@@ -2,7 +2,7 @@ export PATH="/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/git/man:$MANPATH"
 
 if test "$(uname -s)" != "Darwin"; then
-  export PATH="~/.gem/ruby/2.5.0/bin:$PATH"
-  export PATH="~/.local/bin/:$PATH"
+  export PATH="/home/joshua/.gem/ruby/2.5.0/bin:$PATH"
+  export PATH="/home/joshua/.local/bin/:$PATH"
 fi
 
