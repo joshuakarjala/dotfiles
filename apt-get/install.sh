0e58f98e9fcff0ee67a4548241e4bb8ca171ef0a
Joshua Karjala-Svendsen <joshua@fluxuries.com2>
2019-09-09 17:10:48 +0200

    don't install apt-get stuff on OSX

--- apt-get/install.sh
+++ apt-get/install.sh
@@ -4,6 +4,10 @@
 #
 # This installs some of the common dependencies needed with apt-get
 
+if test "$(uname)" = "Darwin"; then
+  exit 0
+fi
+
 sudo apt-get update
 sudo apt-get install -y zsh tmux grc grep emacs coreutils tree ack ctags htop curl wget cmake libboost-all-dev silversearcher-ag libpq-dev thefuck gpg imagemagick ghostscript git-flow direnv httpie libuv1-dev  libunibilium-dev libtermkey-dev libvterm-dev vim neovim python3 ruby fzy python3-setuptools python-setuptools ruby-dev python3-pip python-pip build-essential xdg-utils
 
