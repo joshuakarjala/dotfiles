952d9dcee21d1f0e8bb2bce33b73928f024914f5
Joshua Karjala-Svendsen <joshua@fluxuries.com2>
2019-09-11 14:33:04 +0200

    snap stuff

--- system/_path.zsh
+++ system/_path.zsh
@@ -4,6 +4,7 @@ export MANPATH="/usr/local/man:/usr/local/git/man:$MANPATH"
 if test "$(uname -s)" != "Darwin"; then
   export PATH="/home/joshua/.gem/ruby/2.5.0/bin:$PATH"
   export PATH="/home/joshua/.local/bin/:$PATH"
+  export PATH="/snap/bin/:$PATH"
 else
   export PATH="/usr/local/opt/ruby/bin:$PATH"
 fi
