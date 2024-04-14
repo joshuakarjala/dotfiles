3798b3ae95b6f263d3d7ad15dba9cfff16308204
Joshua Karjala <joshua@fluxuries.com>
2024-04-14 22:03:52 +0200

    now?

--- docker/install.sh
+++ docker/install.sh
@@ -1,7 +1,10 @@
-if test "$(uname -s)" != "Darwin" && ! $(which docker);  then
-  curl -fsSL https://get.docker.com -o get-docker.sh
-  sudo sh get-docker.sh
-  rm get-doocker.sh
-else
-  brew install --cask docker
+if test ! $(command -v docker);
+then
+  if test "$(uname -s)" != "Darwin";  then
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sudo sh get-docker.sh
+    rm get-doocker.sh
+  else
+    brew install --cask docker
+  fi
 fi
