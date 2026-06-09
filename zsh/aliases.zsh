04fde6d22044878fd888a054bdfa1b3819655716
Joshua Karjala <joshua@fluxuries.com>
2026-06-09 21:12:03 +0200

    stuff

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -6,4 +6,5 @@ alias subl='open -a "Sublime Text"'
 alias stree='/Applications/SourceTree.app/Contents/Resources/stree'
 alias ta='tmux attach'
 alias tlocal='tmux attach-session -t local || tmux new-session -s local'
-alias zot="zotify --download-format=mp3"
+alias zot="zotify --audio-format=mp3 --download-quality=very_high"
+alias autopbcopy="autossh -M 0 -f -N joshua@talo.server -R 3451:localhost:3451 -C"
