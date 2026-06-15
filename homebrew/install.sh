3bfe34317f642a2d715c8a0adc63a1e767d7c9b7
Joshua Karjala <joshua@fluxuries.com>
2026-06-15 13:11:14 +0200

    fixed python stuff

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -17,10 +17,9 @@ then
 fi
 
 # Install homebrew packages
-brew tap neovim/neovim
 
-brew install --cask qlcolorcode qlstephen qlmarkdown quicklook-json qlimagesize qlvideo suspicious-package quicklookase qlvideo webpquicklook marta db-browser-for-sqlite ngrok
-brew install zsh tmux grc grep coreutils tree fzf ack ctags fzy htop-osx curl wget cmake boost thefuck neovim vim reattach-to-user-namespace restic rclone rsync libuv unibilium the_silver_searcher gnupg
+brew install --cask suspicious-package qlvideo marta db-browser-for-sqlite ngrok
+brew install zsh tmux grc grep coreutils tree fzf ack ctags fzy htop-osx curl wget cmake boost thefuck neovim vim reattach-to-user-namespace restic rclone rsync libuv unibilium the_silver_searcher gnupg pyenv sshpass
 
 # for catalina
 xattr -d -r com.apple.quarantine ~/Library/QuickLook
